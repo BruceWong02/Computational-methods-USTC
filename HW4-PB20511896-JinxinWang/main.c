23171c944963ef26a469e6d23b1671810dad48f7
BruceWong02 <114544851+BruceWong02@users.noreply.github.com>
2022-10-22 22:36:16 +0800

    .

--- HW4-PB20511896-JinxinWang/main.c
+++ HW4-PB20511896-JinxinWang/main.c
@@ -27,49 +27,49 @@
 int main(){
     double epsilon = 0.000001;
 
-    // // (a)
-    // double **A = MatrixGenerator(4, 3, MatrixInit_Rand_0_1);
-    // double **U = MatrixGenerator(4, 4, MatrixInit_0);
-    // double **Sigma = MatrixGenerator(4, 3, MatrixInit_0);
-    // double **VT = MatrixGenerator(3, 3, MatrixInit_0);
-    // SVD(4, 3, A, U, Sigma, VT, epsilon);
+    // (a)
+    double **A = MatrixGenerator(4, 3, MatrixInit_Rand_0_1);
+    double **U = MatrixGenerator(4, 4, MatrixInit_0);
+    double **Sigma = MatrixGenerator(4, 3, MatrixInit_0);
+    double **VT = MatrixGenerator(3, 3, MatrixInit_0);
+    SVD(4, 3, A, U, Sigma, VT, epsilon);
 
-    // printf("A = U Sigma VT :\n");
-    // for (int i=0;i<4;i++){
-    //     printf("[");
-    //     for (int j=0;j<3;j++){
-    //         printf("%f   ", A[i][j]);
-    //     }
-    //     printf("\b\b\b]");
+    printf("A = U Sigma VT :\n");
+    for (int i=0;i<4;i++){
+        printf("[");
+        for (int j=0;j<3;j++){
+            printf("%f   ", A[i][j]);
+        }
+        printf("\b\b\b]");
 
-    //     printf((1==i ? " = " : "   "));
+        printf((1==i ? " = " : "   "));
 
-    //     printf("[");
-    //     for (int j=0;j<4;j++){
-    //         printf("%f   ", U[i][j]);
-    //     }
-    //     printf("\b\b\b][");
+        printf("[");
+        for (int j=0;j<4;j++){
+            printf("%f   ", U[i][j]);
+        }
+        printf("\b\b\b][");
 
-    //     for (int j=0;j<3;j++){
-    //         printf("%f   ", Sigma[i][j]);
-    //     }
-    //     printf("\b\b\b][");
+        for (int j=0;j<3;j++){
+            printf("%f   ", Sigma[i][j]);
+        }
+        printf("\b\b\b][");
 
-    //     if (3==i){
-    //         printf("\b \n\n");
-    //     }
-    //     else {
-    //         for (int j=0;j<3;j++){
-    //             printf("%f   ", VT[i][j]);
-    //         }
-    //         printf("\b\b\b]\n");
-    //     }
-    // }
+        if (3==i){
+            printf("\b \n\n");
+        }
+        else {
+            for (int j=0;j<3;j++){
+                printf("%f   ", VT[i][j]);
+            }
+            printf("\b\b\b]\n");
+        }
+    }
     
-    // FreeMatrixMemory(A, 4);
-    // FreeMatrixMemory(U, 4);
-    // FreeMatrixMemory(Sigma, 4);
-    // FreeMatrixMemory(VT, 3);
+    FreeMatrixMemory(A, 4);
+    FreeMatrixMemory(U, 4);
+    FreeMatrixMemory(Sigma, 4);
+    FreeMatrixMemory(VT, 3);
 
     // ****************************************************
     // (b)
@@ -92,18 +92,18 @@ int main(){
 
     PCA(4, 150, IrisDatas, epsilon, 2, FinalM, 1);
 
-    // // Create output file.
-    // char *filename1 = "result.csv";
-    // FILE * fp1 = fopen(filename1, "w+");
-    // if (NULL == fp1){
-    //     printf("fopen() failed!!!");
-    //     exit(EXIT_FAILURE);
-    // }
-    // // Output result
-    // for (int i=0;i<150;i++){
-    //     fprintf(fp1, "%f,%f,%d\n", FinalM[0][i], FinalM[1][i], labels[i]);
-    // }
-    // printf((0 == fclose(fp1)) ? "Results saved successfully.\n" : "Results saving failed.");  
+    // Create output file.
+    char *filename1 = "result.csv";
+    FILE * fp1 = fopen(filename1, "w+");
+    if (NULL == fp1){
+        printf("fopen() failed!!!");
+        exit(EXIT_FAILURE);
+    }
+    // Output result
+    for (int i=0;i<150;i++){
+        fprintf(fp1, "%f,%f,%d\n", FinalM[0][i], FinalM[1][i], labels[i]);
+    }
+    printf((0 == fclose(fp1)) ? "Results saved successfully.\n" : "Results saving failed.");  
 
 
     FreeMatrixMemory(FinalM, 2);
